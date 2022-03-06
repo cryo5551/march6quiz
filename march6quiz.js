4bce117844f4f356834f083d7280cf6e34f58c4f
cryo5551 <96884049+cryo5551@users.noreply.github.com>
2022-03-06 19:24:25 +0530

    Add files via upload

---
+++ march6quiz.js
@@ -0,0 +1,47 @@
+function sms20() {
+    document.getElementById("sams20").innerHTML = "<h3>Rs=45999</h3> <p>RAM 6Gb ROM 256Gb</p> <h3>Display</h3><p>Type Dynamic AMOLED 2X, 120Hz,<br> HDR10+, 1400 nits</p>";
+    document.getElementById("sams201").innerHTML = "<p>Size	6.9 in (~89.9% screen-to-body ratio) <br> Resolution	1440 x 3200 20:9 ratio</p>";
+    document.getElementById("sams202").innerHTML = "<p> Protection Corning Gorilla Glass 6 <br> Always-on display <br> 120Hz@FHD/60Hz@QHD refresh rate</p>";
+}
+
+function sms21() {
+    document.getElementById("sams211").innerHTML = "<h3>Rs=69999</h3> <p>RAM 8Gb ROM 526Gb</p> <h3>Display</h3><p>Type Dynamic AMOLED 2X, 120Hz,<br> HDR10+, 1400 nits</p>";
+    document.getElementById("sams212").innerHTML = "<p>Size	7.0 inches, (~90.9% screen-to-body ratio) <br> Resolution	1440 x 3200 20:9 ratio</p>";
+    document.getElementById("sams213").innerHTML = "<p> Protection Corning Gorilla Glass 8 <br> Always-on display <br> 120Hz@FHD/60Hz@QHD refresh rate</p>";
+}
+
+function iqoo() {
+    document.getElementById("iqoo1").innerHTML = "<h3>Rs=49999</h3> <p>RAM 12Gb ROM 256Gb</p> <h3>Display</h3><p>Type Dynamic AMOLED 2X, 120Hz,<br> HDR10+, 1400 nits</p>";
+    document.getElementById("iqoo2").innerHTML = "<p>Size	6.9 inches, (~91.9% screen-to-body ratio) <br> Resolution	1440 x 3200 20:9 ratio</p>";
+    document.getElementById("iqoo3").innerHTML = "<p> Protection Corning Gorilla Glass 8 <br> Always-on display <br> 120Hz@FHD/60Hz@QHD refresh rate</p>";
+}
+
+function mi10t() {
+    document.getElementById("mi101").innerHTML = "<h3>Rs=29999</h3> <p>RAM 6Gb ROM 256Gb</p> <h3>Display</h3><p>Type Dynamic AMOLED 2X, 120Hz,<br> HDR10+, 1400 nits</p>";
+    document.getElementById("mi102").innerHTML = "<p>Size	6.8 inches, (~88.9% screen-to-body ratio) <br> Resolution	1440 x 3200 20:9 ratio</p>";
+    document.getElementById("mi103").innerHTML = "<p> Protection Corning Gorilla Glass 5 <br> Always-on display <br> 120Hz@FHD/60Hz@QHD refresh rate</p>";
+}
+
+function ipn13() {
+    document.getElementById("ip131").innerHTML = "<h3>Rs=129999</h3> <p>RAM 6Gb ROM 256Gb</p> <h3>Display</h3><p>Type Dynamic AMOLED 2X, 120Hz,<br> HDR10+, 1400 nits</p>";
+    document.getElementById("ip132").innerHTML = "<p>Size	6.9 inches, (~94.9% screen-to-body ratio) <br> Resolution	1440 x 3200 20:9 ratio</p>";
+    document.getElementById("ip133").innerHTML = "<p> Protection Corning Gorilla Glass 9 <br> Always-on display <br> 120Hz@FHD/60Hz@QHD refresh rate</p>";
+}
+
+function ipn12() {
+    document.getElementById("ip121").innerHTML = "<h3>Rs=79999</h3> <p>RAM 6Gb ROM 256Gb</p> <h3>Display</h3><p>Type Dynamic AMOLED 2X, 120Hz,<br> HDR10+, 1400 nits</p>";
+    document.getElementById("ip122").innerHTML = "<p>Size	6.9 inches, (~94.9% screen-to-body ratio) <br> Resolution	1440 x 3200 20:9 ratio</p>";
+    document.getElementById("ip123").innerHTML = "<p> Protection Corning Gorilla Glass 9 <br> Always-on display <br> 120Hz@FHD/60Hz@QHD refresh rate</p>";
+}
+
+function op9() {
+    document.getElementById("op91").innerHTML = "<h3>Rs=59999</h3> <p>RAM 6Gb ROM 256Gb</p> <h3>Display</h3><p>Type Dynamic AMOLED 2X, 120Hz,<br> HDR10+, 1400 nits</p>";
+    document.getElementById("op92").innerHTML = "<p>Size	6.9 inches, (~94.9% screen-to-body ratio) <br> Resolution	1440 x 3200 20:9 ratio</p>";
+    document.getElementById("op93").innerHTML = "<p> Protection Corning Gorilla Glass 9 <br> Always-on display <br> 120Hz@FHD/60Hz@QHD refresh rate</p>";
+}
+
+function opnm() {
+    document.getElementById("opn1").innerHTML = "<h3>Rs=31999</h3> <p>RAM 6Gb ROM 256Gb</p> <h3>Display</h3><p>Type Dynamic AMOLED 2X, 120Hz,<br> HDR10+, 1400 nits</p>";
+    document.getElementById("opn2").innerHTML = "<p>Size	6.9 inches, (~94.9% screen-to-body ratio) <br> Resolution	1440 x 3200 20:9 ratio</p>";
+    document.getElementById("opn3").innerHTML = "<p> Protection Corning Gorilla Glass 9 <br> Always-on display <br> 120Hz@FHD/60Hz@QHD refresh rate</p>";
+}
